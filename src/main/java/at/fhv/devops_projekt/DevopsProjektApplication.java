81d8d77a4c628aa1b61d5781462dc0a08b84e310
Emanuel Peter <157378848+Ema645@users.noreply.github.com>
2026-09-02 16:14:22 +0200

    first commit

---
+++ src/main/java/at/fhv/devops_projekt/DevopsProjektApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.devops_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhv/devops_projekt/DevopsProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.devops_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
